d0f5a1187883959229ec60ee4cdea220e6f9919c
暮耘 <wangyuhao.wyh@cainiao.com>
2024-05-22 22:05:50 +0800

    feat: 增加状态栏菜单、增加开启代理、关闭代理功能

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,8 @@ use std::path::Path;
 use std::io::{self, Read};
 use serde_json::{json, Value};
 use std::process::{Command, Stdio};
+use tauri::{SystemTrayMenuItem, SystemTray, CustomMenuItem, SystemTrayMenu, SystemTrayEvent, Menu, MenuItem, Submenu, Window, WindowEvent};
+use tauri::Manager; // 提供窗口管理器相关的功能
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -121,7 +123,50 @@ fn exec_command(path: String, command: String) -> Result<String, String> {
 }
 
 fn main() {
+    // 创建菜单项
+    let quit = CustomMenuItem::new("quit".to_string(), "退出");
+    let enable_proxy = CustomMenuItem::new("enable_proxy".to_string(), "一键开启代理");
+    let disable_proxy = CustomMenuItem::new("disable_proxy".to_string(), "一键关闭代理");
+
+    // 创建系统托盘菜单
+    let tray_menu = SystemTrayMenu::new().add_item(enable_proxy).add_item(disable_proxy).add_native_item(SystemTrayMenuItem::Separator).add_item(quit);
+
+    // 创建系统托盘实例
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(|_app, event| match event {
+                    SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                        "quit" => {
+                            std::process::exit(0);
+                        }
+                        "enable_proxy" => {
+                            // 执行启用代理的命令
+                            if let Ok(_) = Command::new("sh")
+                               .arg("-c")
+                               .arg("networksetup -setwebproxy 'Wi-Fi' host port && networksetup -setsecurewebproxy 'Wi-Fi' host port")
+                               .output() {
+                                    println!("Proxy enabled successfully");
+                               } else {
+                                    println!("Failed to enable proxy");
+                               }
+                        }
+                        "disable_proxy" => {
+                                // 执行关闭代理的命令
+                                if let Ok(_) = Command::new("sh")
+                                    .arg("-c")
+                                    .arg("networksetup -setwebproxystate 'Wi-Fi' off && networksetup -setsecurewebproxystate 'Wi-Fi' off")
+                                    .output() {
+                                        println!("Proxy disable successfully");
+                                    } else {
+                                        println!("Failed to enable proxy");
+                                    }
+                        }
+                        _ => {}
+                    }
+                    _ => {}
+                })
         .invoke_handler(tauri::generate_handler![greet, read_directory, read_package_json_files, exec_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
